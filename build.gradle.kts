db1e904c507bedbbf9b9b9d9fc8b0d3165ec3a46
djcostin <daniel.costin@gmail.com>
2025-01-20 16:41:54 +0000

    DPR2-1510 try removing athena dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
   implementation("com.google.guava:guava:33.3.1-jre")
 
   implementation("software.amazon.awssdk:redshiftdata:2.29.20")
-  implementation("software.amazon.awssdk:athena:2.29.20")
+  //implementation("software.amazon.awssdk:athena:2.29.20")
   implementation("aws.sdk.kotlin:dynamodb:1.3.90")
 
   // Testing
