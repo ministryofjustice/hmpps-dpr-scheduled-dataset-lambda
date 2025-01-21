ba15c8a34792916e4e229c53d7220e5d939f09d8
djcostin <daniel.costin@gmail.com>
2025-01-21 16:52:58 +0000

    DPR2-1510 aws protocols

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
   implementation("aws.sdk.kotlin:aws-http-jvm:1.3.90")
   implementation("aws.smithy.kotlin:http-auth-aws:1.3.28")
   implementation("aws.smithy.kotlin:http-auth-aws-jvm:1.3.28")
+  implementation("aws.smithy.kotlin:aws-json-protocols:1.3.28")
 
   // Testing
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
