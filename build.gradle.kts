9a740ff0fe299704b8623b0505daa6c7e55f2c24
djcostin <daniel.costin@gmail.com>
2025-01-22 10:40:57 +0000

    DPR2-1510 update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
   //implementation("aws.sdk.kotlin:dynamodb:1.3.90")
 
   implementation("jakarta.activation:jakarta.activation-api:2.1.0")
+  implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
   implementation("org.apache.httpcomponents:httpcore:4.4.16")
   implementation("jakarta.activation:jakarta.activation-api:2.1.0")
   implementation("org.slf4j:slf4j-api:2.0.16")
