8732cd45ba52a5b680da83a36ffc3f17d23543d1
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-04-05 19:48:06 +0100

    DPR2-1873 wip - attempt to run end to end process

--- src/main/kotlin/uk/gov/justice/digital/hmpps/scheduled/lambda/ManageAthenaAsyncQueries.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/scheduled/lambda/ManageAthenaAsyncQueries.kt
@@ -4,10 +4,8 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.LambdaLogger
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.logging.LogLevel
-import org.checkerframework.checker.units.qual.t
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.athena.AthenaClient
-import software.amazon.awssdk.services.athena.model.Database
 import software.amazon.awssdk.services.athena.model.QueryExecutionContext
 import software.amazon.awssdk.services.athena.model.StartQueryExecutionRequest
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient
@@ -31,18 +29,18 @@ class ManageAthenaAsyncQueries : RequestHandler<MutableMap<String, Any>, String>
               logger.log("No execution ID or current state found in the Event.", LogLevel.ERROR)
               throw RuntimeException("No execution ID or current state found in the Event.")
           }
-          val updateStateQuery = "UPDATE datamart.admin.execution_manager SET current_state = $currentState WHERE execution_id = $queryExecutionId"
+          val updateStateQuery = "UPDATE datamart.admin.execution_manager SET current_state = $currentState WHERE current_execution_id = $queryExecutionId"
           queryRedshift(updateStateQuery, logger)
           if (currentState == "SUCCEEDED") {
-              val adminQuery = """
-                 SELECT t1.query, t1.database, t1.catalog, t1.datasource FROM
-                  (SELECT root_execution_id, query, index, database, catalog, datasource FROM datamart.admin.execution_manager WHERE execution_id = '$queryExecutionId') AS t1
+              val nextQueryToRun = """
+                 SELECT t2.query, t2.database, t2.catalog, t2.datasource FROM
+                  (SELECT root_execution_id, query, index FROM datamart.admin.execution_manager WHERE current_execution_id = '$queryExecutionId') AS t1
                   JOIN
-                  (SELECT root_execution_id, index FROM datamart.admin.execution_manager) AS t2
+                  (SELECT root_execution_id, index, query, database, catalog, datasource  FROM datamart.admin.execution_manager) AS t2
                  ON t1.root_execution_id = t2.root_execution_id AND t2.index = (t1.index + 1)
                   """
               logger.log("Running admin query", LogLevel.INFO)
-              val getStatementResultResponse = queryRedshift(adminQuery, logger)
+              val getStatementResultResponse = queryRedshift(nextQueryToRun, logger)
               if (getStatementResultResponse.totalNumRows() == 1L) {
                   val database = getData("database", 0, getStatementResultResponse)
                   logger.log("The database from the admin table is: $database", LogLevel.INFO)
@@ -61,7 +59,7 @@ class ManageAthenaAsyncQueries : RequestHandler<MutableMap<String, Any>, String>
           } else if (currentState == "FAILED" || currentState == "CANCELLED") {
             val error = ((payload["detail"] as Map<String,Any>)["athenaError"] as Map<String,Any>)["errorMessage"] as String
             logger.log("Query with execution ID: $queryExecutionId failed. Error: $error",LogLevel.ERROR)
-            val updateStateQuery = "UPDATE datamart.admin.execution_manager SET current_state = $currentState, error = '$error'  WHERE execution_id = '$queryExecutionId'"
+            val updateStateQuery = "UPDATE datamart.admin.execution_manager SET current_state = $currentState, error = '$error'  WHERE current_execution_id = '$queryExecutionId'"
             queryRedshift(updateStateQuery, logger)
           }
     }
@@ -72,7 +70,7 @@ class ManageAthenaAsyncQueries : RequestHandler<MutableMap<String, Any>, String>
         val statementRequest = ExecuteStatementRequest.builder()
             .clusterIdentifier("dpr-redshift-development")
             .database("datamart")
-            .secretArn("")
+            .secretArn("arn:aws:secretsmanager:eu-west-2:771283872747:secret:dpr-redshift-secret-development-rLHcQZ")
             .sql(query)
             .build()
 //        parameters?.let {
