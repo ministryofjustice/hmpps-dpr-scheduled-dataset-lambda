8c98c6936ad66145871e7a5de4b95a4cc6e056c2
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2025-04-09 16:57:38 +0100

    DPR2-1873 remove surrounding quotes from base64 decode

--- src/main/kotlin/uk/gov/justice/digital/hmpps/scheduled/lambda/ManageAthenaAsyncQueries.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/scheduled/lambda/ManageAthenaAsyncQueries.kt
@@ -47,7 +47,7 @@ class ManageAthenaAsyncQueries : RequestHandler<MutableMap<String, Any>, String>
               if (getStatementResultResponse.totalNumRows() == 1L) {
                   val rootExecutionId = getData("root_execution_id", 0, getStatementResultResponse)
                   val index = getIntData("index", 0, getStatementResultResponse)
-                  val query = String(Base64.getDecoder().decode(getData("query", 0, getStatementResultResponse).toByteArray()))
+                  val query = String(Base64.getDecoder().decode(getData("query", 0, getStatementResultResponse).removeSurrounding("\"").toByteArray()))
                   logger.log("Retrieved ${getStatementResultResponse.records()} results from admin table.", LogLevel.INFO)
                   val datasource = getData("datasource", 0, getStatementResultResponse)
                   if (datasource == "redshift") {
